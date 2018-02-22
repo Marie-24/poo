746f3151d41a9ef84e3d9b2a8f25efbe3293c934
Marie-24 <mariepierre.dupoux@gmail.com>
2018-02-22 16:12:42 +0100

    premier push

---
+++ Livre.php
@@ -0,0 +1,58 @@
+<?php
+class Livre{
+
+	public $format;
+	public $genre ;
+	public $auteur;
+	public $annee ;
+	public $ref;
+	public $extrait;
+
+
+
+	public function auteurContemporain($annee,$auteur){
+			if($annee>1930){
+				return "".$auteur." est contemporain<br>";
+			}
+			else{
+				return "c'est un vieux<br>";
+			}
+
+	}
+	public function searchWord($word){
+		$word_position = strpos($this->extrait,$word);
+		if($word_position !==false){
+			return "Le livre contient le mot <strong>".$word."</strong> à la position ".$word_position."<br>";
+		}
+		else {
+			return "Le livre ne contient pas ".$word."<br>";
+		}
+
+	}
+
+	public function getTitle($titre){
+		return "Le titre du livre est <em> ".$titre."</em><br>";
+
+
+	}
+
+}
+
+
+$livre = new livre();
+$totor = $livre->auteur ="Dahl";
+$intitule = $livre->titre = "Bizarre";
+$livre->extrait = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pulvinar in dolor a bibendum. Maecenas ut varius risus. Duis ullamcorper mollis magna, sed lobortis felis tempor quis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus vel venenatis quam. Vestibulum interdum scelerisque dui, at efficitur dolor elementum vehicula. Maecenas eu cursus metus, id mollis justo. Donec accumsan mauris in consequat dignissim. Curabitur ornare non metus in aliquam. Pellentesque tincidunt molestie justo porta eleifend. Duis laoreet egestas arcu. Curabitur et justo ullamcorper augue tincidunt dictum.";
+
+
+$annee_tamere = $livre->annee=1950;
+
+
+
+echo $livre->searchWord('mollis');
+echo $livre->auteurContemporain($annee_tamere,$totor);
+echo $livre->getTitle($intitule);
+
+echo"<pre>";
+print_r ($livre);
+echo"</pre>";
